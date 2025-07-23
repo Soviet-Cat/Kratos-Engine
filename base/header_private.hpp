b488795fe08c5ce8589881d344847f2020f74c5c
Soviet Cat <161828581+Soviet-Cat@users.noreply.github.com>
2025-07-23 10:01:45 +1000

    Added Create Script and Framework Target in CMakeLists.txt

---
+++ base/header_private.hpp
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------
+   Copyright (c) 2025 Soviet-Cat. All Rights Reserved.
+   See LICENSE file for details.
+   --------------------------------------------------- */
+
+#pragma once
+
+namespace Kratos
+{
+    
+}
---
+++ base/source.cpp
@@ -0,0 +1,4 @@
+/* ---------------------------------------------------
+   Copyright (c) 2025 Soviet-Cat. All Rights Reserved.
+   See LICENSE file for details.
+   --------------------------------------------------- */
